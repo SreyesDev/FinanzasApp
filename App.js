e42c91ede1b97b2d09731deaf963587bc1d552a3
Samuel Reyes Silverio <reyesamuel999@gmail.com>
2025-09-25 21:55:07 -0400

    feat: agregar archivo App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SafeAreaView, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <SafeAreaView>
+        <Text>Hello, World!</Text>
+      </SafeAreaView>
+    </View>
+  );
+};
+
+export default App;
