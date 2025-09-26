b457994250c564ca0c9fcac5aa8b879782c3944c
Samuel Reyes Silverio <reyesamuel999@gmail.com>
2025-09-26 14:18:04 -0400

    feat: creando estructura para los routes de navegacion de las pantallas

---
+++ src/container/root/routes/DrawerRouetes.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const DrawerRouetes = () => {
+  return (
+    <div>DrawerRouetes</div>
+  )
+}
---
+++ src/container/root/routes/TabRoutes.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const TabRoutes = () => {
+  return (
+    <div>TabRoutes</div>
+  )
+}
